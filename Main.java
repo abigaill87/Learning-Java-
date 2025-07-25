51e7160c07b8411752530a1d053cdd06a4e1d154
abigailliao320@gmail.com <abigailliao320@gmail.com>
2025-07-24 23:33:55 -0400

    finished all prac problems

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-    int[] arr = {4, 2, 3, 4, 1, 3, 4, 2, 1, 3, 292};
-        System.out.println(Practiceproblems.countOccurences(arr, 3));
+        int[] arr = {4, 2, 3, 4, 1, 3, 4, 2, 1, 3, 292};
     }
 }
--- Practiceproblems.java
+++ Practiceproblems.java
@@ -1,5 +1,5 @@
 public class Practiceproblems {
-
+    // Practice Problem 1
     static int countOccurences(int[] arr, int n) {
         int x = 0;
         for (int i : arr) {
@@ -9,4 +9,37 @@ public class Practiceproblems {
         }
         return x;
     }
+
+    // Practice Problem 2
+    static int[] reverseArray(int[] arr) {
+        int[] revArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            revArr[i] = arr[arr.length - 1 - i];
+        }
+        return revArr;
+    }
+
+    // Practice Problem 3
+    static double sumGrid(double[][] grid) {
+        double x = 0.0;
+        for (int i = 0; i < grid[i].length; i++) {
+            for (int j = 0; j < grid[j].length; j++) {
+                x += grid[i][j];
+            }
+        }
+        return x;
+    }
+
+    // Practice Problem 4
+    static int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        for (int i = 2; i <= n; i++) {
+            int[] fib = new int[n+1];
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+          
+    }return fib[n];
 }
